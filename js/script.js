33d99f57e2f0cb79c3f381b86ed97e47d8e5225d
hyle_s <samhyle@gmail.com>
2023-10-07 22:30:43 -0500

    Added functions to accept and validate player guesses

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -8,7 +8,9 @@ const guessMessage = document.querySelector(".message");
 const playAgainButton = document.querySelector("#play again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
+//Placeholder symbols for word's letters
 const placeholder = function (word) {
     const placeholderLetters = [];
     for (const letter of word) {
@@ -23,7 +25,39 @@ placeholder(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
+    //empty message
+    guessMessage.innerText = "";
     const guess = letterInput.value; 
-    console.log(guess);
+    const goodGuess = validateInput(guess);
+
+    if (goodGuess) {
+        makeGuess(guess);
+      }
+
     letterInput.value = "";
+
 });
+
+//Making sure the user inputs a single letter, from A to Z
+const validateInput = function (input)  {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        guessMessage.innerText = "Please enter a letter.";
+    } else if (input.length > 1) {
+        guessMessage.innerText = "Please enter a single letter.";
+    } else if (!input.match(acceptedLetter)) {
+        guessMessage.innerText = "Please enter a letter from A to Z.";
+    }   else {
+        return input;
+    };
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        guessMessage.innerText = "You already guessed that one! Try again!";
+    }   else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
